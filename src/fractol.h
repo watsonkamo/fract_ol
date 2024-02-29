3166d2f2917b1745e8c9bdb95923e441f2a77454
watosonkamo <umdroid.1917@gmail.com>
2024-03-01 03:14:14 +0900

    first commit

---
+++ src/fractol.h
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eshintan <eshintan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 03:11:33 by eshintan          #+#    #+#             */
+/*   Updated: 2024/03/01 02:26:17 by eshintan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACT_OL_H
+# define FRACT_OL_H
+
+//# include "libft.h"
+# include "minilibx-linux/mlx.h"
+# include <math.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+//image
+//this is basically a pixels buffer
+//values from mlx_get_data_addr
+//mlx_get_data_addr returns a pointer to the image data
+typedef struct s_image
+{
+    void    *img_ptr;//pointer to the image
+    char    *addr;//pointer to the first pixel in the image
+    int     bits_per_pixel;//number of bits needed to represent a pixel color
+    int     line_len;//size of a line in bytes
+    int     endian;//endianness of the image
+}   t_image;
+
+//fractal
+//mlxstuff
+//this is the main struct that holds all the mlx stuff
+
+typedef struct s_fractal
+{
+    //MLX
+    void    *mlx_start;
+    void    *mlx_window;
+    //image
+    t_image img;
+    //hooks member variables
+}               t_fractal;
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	ft_putstr_fd(char *s, int fd);
+size_t	ft_strlen(const char *str);
+void	draw_fractal(t_fractal *fractal);
+static void	malloc_error(void);
+# define WIDTH 800
+
+# define HEIGHT 800
+
+# define MAX_ITER 100
+
+# define ZOOM 1.1
+
+# define MOVE 0.1
+
+# define RED 0x00FF0000
+
+# define GREEN 0x0000FF00
+
+# define BLUE 0x000000FF
+
+# define WHITE 0x00FFFFFF
+
+# define BLACK 0x00000000
+
+# define YELLOW 0x00FFFF00
+
+# define PURPLE 0x00FF00FF
+
+# define CYAN 0x0000FFFF
+
+# define ORANGE 0x00FFA500
+
+# define PINK 0x00FFC0CB
+
+# define BROWN 0x00A52A2A
+
+# define GREY 0x00808080
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eshintan <eshintan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 02:37:58 by eshintan          #+#    #+#             */
+/*   Updated: 2024/03/01 02:43:56 by eshintan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "fractol.h"
+// #include "libft/libft.h"
+#include "fractol.h"
+
+static void	malloc_error(void)
+{
+	ft_putstr_fd("Error\nMalloc error\n", 2);
+	exit(1);
+}
+
+int	main(int ac, char **av)
+{
+	t_fractal	fractal;
+
+	if (ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10))
+	{
+		draw_fractal(&fractal);
+	}
+	else if (ac == 2 && !ft_strncmp(av[1], "julia", 5))
+	{
+		draw_fractal(&fractal);
+	}
+	else
+	{
+		ft_putstr_fd("Usage: ./fractol [mandelbrot/julia]\n", 2);
+		return (0);
+	}
+}
+
+void	draw_fractal(t_fractal *fractal)
+{
+		fractal->mlx_start = mlx_init();
+		if (NULL == fractal->mlx_start)
+        {
+            malloc_error();
+        }
+		fractal->mlx_window = mlx_new_window(fractal->mlx_start, WIDTH, HEIGHT, "fractol");
+        if (NULL == fractal->mlx_window)
+        {
+            mlx_destroy_window(fractal->mlx_start, fractal->mlx_window);
+            free(fractal->mlx_start);
+            malloc_error();
+        }
+		fractal->img.img_ptr = mlx_new_image(fractal->mlx_start, WIDTH, HEIGHT);
+        if (NULL == fractal->img.img_ptr)
+        {
+            mlx_destroy_window(fractal->mlx_start, fractal->mlx_window);
+            //mlx_destroy_display(fractal->mlx_start);
+            free(fractal->mlx_start);
+            malloc_error();
+        }
+		fractal->img.addr = mlx_get_data_addr(fractal->img.img_ptr, &fractal->img.bits_per_pixel, &fractal->img.line_len, &fractal->img.endian);
+		mlx_put_image_to_window(fractal->mlx_start, fractal->mlx_window, fractal->img.img_ptr, 0, 0);
+		mlx_loop(fractal->mlx_start);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	while (n > 0)
+	{
+		if (*s1 != *s2 || *s1 == '\0' || *s2 == '\0')
+		{
+			return ((unsigned char)(*s1) - (unsigned char)(*s2));
+		}
+		++s1;
+		++s2;
+		--n;
+	}
+	return (0);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+//rendering fractals
+

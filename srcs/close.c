b3e8a0d0ffd396531492084f296c6e43b351eed7
watsonkamo <umdroid.1917@gmail.com>
2024-05-08 15:28:18 +0900

    fix norm

--- srcs/close.c
+++ srcs/close.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   buttun.c                                           :+:      :+:    :+:   */
+/*   close.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emma <emma@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 04:01:55 by emma              #+#    #+#             */
-/*   Updated: 2024/04/26 04:02:52 by emma             ###   ########.fr       */
+/*   Updated: 2024/05/08 15:16:22 by emma             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int close_esc_buttun(int key, t_fractal *fractal)
+int	close_esc_buttun(int key, t_fractal *fractal)
 {
 	if (key == 53)
 	{
@@ -22,7 +22,7 @@ int close_esc_buttun(int key, t_fractal *fractal)
 	return (0);
 }
 
-int close_window_buttun(t_fractal *fractal)
+int	close_window_buttun(t_fractal *fractal)
 {
 	mlx_destroy_window(fractal->mlx_start, fractal->mlx_window);
 	exit(0);
--- srcs/zoom.c
+++ srcs/zoom.c
@@ -6,15 +6,14 @@
 /*   By: emma <emma@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 04:01:11 by emma              #+#    #+#             */
-/*   Updated: 2024/04/28 23:59:28 by emma             ###   ########.fr       */
+/*   Updated: 2024/05/08 15:27:56 by emma             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void mouse_zoom_in(t_fractal *fractal)
+void	mouse_zoom_in(t_fractal *fractal)
 {
-	//(void)isjulia;
 	printf("zoom:%f\n", fractal->zoom);
 	if (fractal->zoom > 0.1)
 		fractal->zoom *= 0.9;
@@ -22,24 +21,21 @@ void mouse_zoom_in(t_fractal *fractal)
 	draw_fractal(fractal, fractal->isjulia);
 }
 
-void mouse_zoom_out(t_fractal *fractal)
+void	mouse_zoom_out(t_fractal *fractal)
 {
-	//(void)isjulia;
 	printf("zoom:%f\n", fractal->zoom);
 	fractal->zoom *= 1.1;
 	mlx_destroy_image(fractal->mlx_start, fractal->img.img_ptr);
 	draw_fractal(fractal, fractal->isjulia);
 }
 
-int mouse_zoom_hook(int mouse, int x, int y, t_fractal *fractal)
+int	mouse_zoom_hook(int mouse, int x, int y, t_fractal *fractal)
 {
-	(void)x;
-	(void)y;
-	//zoom_in_function
+	(void) x;
+	(void) y;
 	if (mouse == 4)
 		mouse_zoom_in(fractal);
-	//zoom_out_function
 	if (mouse == 5)
 		mouse_zoom_out(fractal);
-	return(0);
+	return (0);
 }
